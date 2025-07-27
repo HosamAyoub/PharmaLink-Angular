b490fc4758caf32238c7d8aaf70d0e77e9d1d9fc
Hosam Ayoub <hosamayoub50@gmail.com>
2025-07-28 02:49:42 +0300

    Remove unused authentication files and components, including auth.interceptor, auth.service, login, and register modules.

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -3,11 +3,15 @@ import { inject, Injectable, signal } from '@angular/core';
 import { API_BASE_URL } from '../api.constants';
 import { BehaviorSubject, catchError, tap, throwError } from 'rxjs';
 import { ResponseData, User } from './user.model';
+import { Router } from '@angular/router';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
   user = signal<User | null>(null);
   http = inject(HttpClient);
+  router = inject(Router);
+  private tokenExpirationDuration: any;
+
   signUp(
     userName: string,
     email: string,
@@ -47,12 +51,28 @@ export class AuthService {
           );
           this.user.set(user);
           localStorage.setItem('userData', JSON.stringify(user));
+          const expirationDate = new Date(resData.value.expiration);
+          const now = new Date();
+          const expirationDuration = expirationDate.getTime() - now.getTime();
+          this.autoLogout(expirationDuration);
         })
       );
   }
 
   logout() {
     this.user.set(null);
+    this.router.navigate(['/client/home']);
+    localStorage.removeItem('userData');
+    if (this.tokenExpirationDuration) {
+      clearTimeout(this.tokenExpirationDuration);
+    }
+    this.tokenExpirationDuration = null;
+  }
+
+  autoLogout(expirationDuration: number) {
+    this.tokenExpirationDuration = setTimeout(() => {
+      this.logout();
+    }, expirationDuration);
   }
 
   autoLogin() {
@@ -68,6 +88,10 @@ export class AuthService {
       );
       if (loadedUser.token) {
         this.user.set(loadedUser);
+        const expirationDate = new Date(userData._expiration);
+        const now = new Date();
+        const expirationDuration = expirationDate.getTime() - now.getTime();
+        this.autoLogout(expirationDuration);
       }
     }
   }
