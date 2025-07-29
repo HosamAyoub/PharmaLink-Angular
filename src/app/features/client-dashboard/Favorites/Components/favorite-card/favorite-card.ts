f12ee0ef2178c938d753659a67d152cca1af563b
Mariem Sameh <maryamsameh61@gmail.com>
2025-07-30 00:32:53 +0300

    Remove description from fav & add link to go to drug details

--- src/app/features/client-dashboard/Favorites/Components/favorite-card/favorite-card.ts
+++ src/app/features/client-dashboard/Favorites/Components/favorite-card/favorite-card.ts
@@ -1,10 +1,11 @@
 import { Component, EventEmitter, input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IFavDrug } from '../../../../../core/drug/IFavDrug';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-favorite-card',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './favorite-card.html',
   styleUrl: './favorite-card.css'
 })
