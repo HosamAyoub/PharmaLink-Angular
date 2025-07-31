5e690bd7d43c4a259cdb978822e97e6739dcc765
Hosam Ayoub <hosamayoub50@gmail.com>
2025-07-31 19:00:18 +0300

    before merging main

--- src/app/features/client-dashboard/Pharmacies/nearby-pharmacies-page/nearby-pharmacies-page.ts
+++ src/app/features/client-dashboard/Pharmacies/nearby-pharmacies-page/nearby-pharmacies-page.ts
@@ -7,38 +7,40 @@ import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-nearby-pharmacies-page',
-  imports: [PharmacyCard,RouterModule],
+  imports: [PharmacyCard, RouterModule],
   templateUrl: './nearby-pharmacies-page.html',
-  styleUrl: './nearby-pharmacies-page.css'
+  styleUrl: './nearby-pharmacies-page.css',
 })
-export class NearbyPharmaciesPage
- implements OnInit {
+export class NearbyPharmaciesPage implements OnInit {
   protected pharmacies: Ipharmacy[] = [];
   protected errorMessage: string | null = null;
   protected Loading: boolean = true;
 
-  constructor(private pharmacyService: PharmacyService, private cdr:ChangeDetectorRef) { }
+  constructor(
+    private pharmacyService: PharmacyService,
+    private cdr: ChangeDetectorRef
+  ) {}
   ngOnInit() {
     this.loadNearbyPharmacies();
   }
 
- loadNearbyPharmacies(): void {
-  this.Loading = true;
-  this.errorMessage = null;
+  loadNearbyPharmacies(): void {
+    this.Loading = true;
+    this.errorMessage = null;
 
-  this.pharmacyService.getPharmacies().subscribe({
-    next: (data) => {
-      this.pharmacies = data;
-      console.log('Nearby pharmacies loaded:', this.pharmacies);
-      this.Loading = false;
-      this.cdr.detectChanges(); // Ensure the view updates immediately
-    },
-    error: (err) => {
-
-      this.errorMessage = 'Failed to load pharmacies. Please try again later.';
-      this.Loading = false;
-      this.cdr.detectChanges();
-    }
-  });
+    this.pharmacyService.getPharmacies().subscribe({
+      next: (data) => {
+        this.pharmacies = data;
+        console.log('Nearby pharmacies loaded:', this.pharmacies);
+        this.Loading = false;
+        this.cdr.detectChanges(); // Ensure the view updates immediately
+      },
+      error: (err) => {
+        this.errorMessage =
+          'Failed to load pharmacies. Please try again later.';
+        this.Loading = false;
+        this.cdr.detectChanges();
+      },
+    });
   }
 }
