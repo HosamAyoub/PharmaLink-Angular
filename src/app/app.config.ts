4dff720a2a07a766259c2e8a151f3cf321e3baf6
Hosam Ayoub <hosamayoub50@gmail.com>
2025-08-01 23:23:57 +0300

    edit sidebar styles

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,15 +5,19 @@ import {
 } from '@angular/core';
 import { routes } from './app.routes';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withInterceptors, withFetch } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withInterceptors,
+  withFetch,
+} from '@angular/common/http';
 // import { authInterceptor } from './interceptors/auth-interceptor';
-import { authInterceptor } from './shared/interceptors/auth.interceptor';
+import { authInterceptor } from './shared/interceptors/auth-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes),
-    provideHttpClient(withFetch(), withInterceptors([authInterceptor]))
+    provideHttpClient(withFetch(), withInterceptors([authInterceptor])),
   ],
 };
