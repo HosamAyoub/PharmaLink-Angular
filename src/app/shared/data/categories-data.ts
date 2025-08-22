ac8d48b654fc78c767e368c30039146af1ef4c6f
Mahmoud22620 <114251031+Mahmoud22620@users.noreply.github.com>
2025-08-22 03:58:14 +0300

    Change Categories Icons

--- src/app/shared/data/categories-data.ts
+++ src/app/shared/data/categories-data.ts
@@ -1,287 +1,282 @@
 export const CategoriesData = [
-
   {
-    name: 'Antibiotic',
-    image: '/assets/images/icons/antibiotic.png',
-    description: '120+ products'
+    "name": "Antibiotic",
+    "image": "https://cdn-icons-png.flaticon.com/512/4320/4320354.png",
+    "description": "120+ products"
   },
   {
-    name: 'Anemia',
-    image: 'assets/images/icons/anemia.png',
-    description: '150+ medicines'
+    "name": "Anemia",
+    "image": "https://cdn-icons-png.flaticon.com/512/2333/2333878.png",
+    "description": "150+ medicines"
   },
   {
-    name: 'Muscle relaxants',
-    image: 'assets/images/icons/muscle.png',
-    description: '10+ products'
+    "name": "Muscle relaxants",
+    "image": "https://cdn-icons-png.flaticon.com/512/7097/7097592.png",
+    "description": "10+ products"
   },
   {
-    name: 'Sleep aids',
-    image: 'assets/images/icons/sleeping-pills.png',
-    description: '15+ products'
+    "name": "Sleep aids",
+    "image": "https://cdn-icons-png.flaticon.com/512/838/838561.png",
+    "description": "15+ products"
   },
   {
-    name: 'Anthelmintics',
-    image: 'https://cdn-icons-png.flaticon.com/512/1143/1143553.png',
-    description: '45+ items'
+    "name": "Anthelmintics",
+    "image": "https://cdn-icons-png.flaticon.com/512/2947/2947943.png",
+    "description": "45+ items"
   },
   {
-    name: 'Vaccine',
-    image: 'assets/images/icons/syringe-svgrepo-com.svg',
-    description: '20+ vaccines'
+    "name": "Vaccine",
+    "image": "https://cdn-icons-png.flaticon.com/512/2785/2785821.png",
+    "description": "20+ vaccines"
   },
   {
-    name: 'Anti-Parkinson',
-    image: 'https://cdn-icons-png.flaticon.com/512/4290/4290854.png',
-    description: '30+ medicines'
+    "name": "Anti-Parkinson",
+    "image": "https://cdn-icons-png.flaticon.com/512/4320/4320334.png",
+    "description": "30+ medicines"
   },
   {
-    name: 'Anti-obesity',
-    image: 'https://cdn-icons-png.flaticon.com/512/3307/3307196.png',
-    description: '25+ supplements'
+    "name": "Anti-obesity",
+    "image": "https://cdn-icons-png.flaticon.com/512/2965/2965570.png",
+    "description": "25+ supplements"
   },
   {
-    name: 'Antiarrhythmics',
-    image: 'https://cdn-icons-png.flaticon.com/512/254/254022.png',
-    description: '40+ medicines'
-  },
-
-  {
-    name: 'Anticancer',
-    image: 'https://cdn-icons-png.flaticon.com/512/4388/4388166.png',
-    description: '60+ medicines'
+    "name": "Antiarrhythmics",
+    "image": "https://cdn-icons-png.flaticon.com/512/833/833472.png",
+    "description": "40+ medicines"
   },
   {
-    name: 'Anticoagulant',
-    image: 'https://cdn-icons-png.flaticon.com/512/2751/2751725.png',
-    description: '35+ medicines'
+    "name": "Anticancer",
+    "image": "https://cdn-icons-png.flaticon.com/512/2965/2965584.png",
+    "description": "60+ medicines"
   },
   {
-    name: 'Antidepressants',
-    image: 'https://cdn-icons-png.flaticon.com/512/7660/7660378.png',
-    description: '80+ products'
+    "name": "Anticoagulant",
+    "image": "https://cdn-icons-png.flaticon.com/512/2947/2947949.png",
+    "description": "35+ medicines"
   },
   {
-    name: 'Antidiabetic',
-    image: 'https://cdn-icons-png.flaticon.com/512/3154/3154587.png',
-    description: '90+ medicines'
+    "name": "Antidepressants",
+    "image": "https://cdn-icons-png.flaticon.com/512/4320/4320320.png",
+    "description": "80+ products"
   },
   {
-    name: 'Antidiarrheals',
-    image: 'https://cdn-icons-png.flaticon.com/512/4689/4689162.png',
-    description: '25+ items'
+    "name": "Antidiabetic",
+    "image": "https://cdn-icons-png.flaticon.com/512/4320/4320333.png",
+    "description": "90+ medicines"
   },
   {
-    name: 'Antidotes',
-    image: 'https://cdn-icons-png.flaticon.com/512/616/616408.png',
-    description: '15+ products'
+    "name": "Antidiarrheals",
+    "image": "https://cdn-icons-png.flaticon.com/512/2910/2910768.png",
+    "description": "25+ items"
   },
   {
-    name: 'Antiemetics',
-    image: 'https://cdn-icons-png.flaticon.com/512/2965/2965573.png',
-    description: '20+ medicines'
+    "name": "Antidotes",
+    "image": "https://cdn-icons-png.flaticon.com/512/2947/2947929.png",
+    "description": "15+ products"
   },
   {
-    name: 'Antiepileptics',
-    image: 'https://cdn-icons-png.flaticon.com/512/831/831682.png',
-    description: '50+ medicines'
+    "name": "Antiemetics",
+    "image": "https://cdn-icons-png.flaticon.com/512/4320/4320341.png",
+    "description": "20+ medicines"
   },
   {
-    name: 'Antifungals',
-    image: 'https://cdn-icons-png.flaticon.com/512/2884/2884565.png',
-    description: '40+ products'
+    "name": "Antiepileptics",
+    "image": "https://cdn-icons-png.flaticon.com/512/4149/4149678.png",
+    "description": "50+ medicines"
   },
   {
-    name: 'Antigout',
-    image: 'https://cdn-icons-png.flaticon.com/512/3523/3523063.png',
-    description: '10+ medicines'
+    "name": "Antifungals",
+    "image": "https://cdn-icons-png.flaticon.com/512/4320/4320330.png",
+    "description": "40+ products"
   },
   {
-    name: 'Antihistamines',
-    image: 'https://cdn-icons-png.flaticon.com/512/10746/10746929.png',
-    description: '60+ products'
+    "name": "Antigout",
+    "image": "https://cdn-icons-png.flaticon.com/512/4149/4149681.png",
+    "description": "10+ medicines"
   },
   {
-    name: 'Antihypertensive',
-    image: 'https://cdn-icons-png.flaticon.com/512/9953/9953296.png',
-    description: '70+ medicines'
+    "name": "Antihistamines",
+    "image": "https://cdn-icons-png.flaticon.com/512/4390/4390313.png",
+    "description": "60+ products"
   },
   {
-    name: 'Antimalarials',
-    image: 'https://cdn-icons-png.flaticon.com/512/4727/4727499.png',
-    description: '15+ medicines'
+    "name": "Antihypertensive",
+    "image": "https://cdn-icons-png.flaticon.com/512/2936/2936869.png",
+    "description": "70+ medicines"
   },
   {
-    name: 'Antiparasitics',
-    image: 'https://cdn-icons-png.flaticon.com/512/4472/4472967.png',
-    description: '20+ products'
+    "name": "Antimalarials",
+    "image": "https://cdn-icons-png.flaticon.com/512/4320/4320324.png",
+    "description": "15+ medicines"
   },
   {
-    name: 'Antiperspirants',
-    image: 'https://cdn-icons-png.flaticon.com/512/2965/2965567.png',
-    description: '10+ items'
+    "name": "Antiparasitics",
+    "image": "https://cdn-icons-png.flaticon.com/512/4320/4320335.png",
+    "description": "20+ products"
   },
   {
-    name: 'Antipsychotics',
-    image: 'https://cdn-icons-png.flaticon.com/512/1143/1143553.png',
-    description: '35+ medicines'
+    "name": "Antiperspirants",
+    "image": "https://cdn-icons-png.flaticon.com/512/4320/4320345.png",
+    "description": "10+ items"
   },
   {
-    name: 'Antiulcer drugs',
-    image: 'https://cdn-icons-png.flaticon.com/512/4290/4290854.png',
-    description: '25+ medicines'
+    "name": "Antipsychotics",
+    "image": "https://cdn-icons-png.flaticon.com/512/4320/4320334.png",
+    "description": "35+ medicines"
   },
   {
-    name: 'Antiviral',
-    image: 'https://cdn-icons-png.flaticon.com/512/3307/3307196.png',
-    description: '30+ products'
+    "name": "Antiulcer drugs",
+    "image": "https://cdn-icons-png.flaticon.com/512/4390/4390310.png",
+    "description": "25+ medicines"
   },
   {
-    name: 'Anxiolytics',
-    image: 'https://cdn-icons-png.flaticon.com/512/254/254022.png',
-    description: '20+ medicines'
+    "name": "Antiviral",
+    "image": "https://cdn-icons-png.flaticon.com/512/2785/2785819.png",
+    "description": "30+ products"
   },
   {
-    name: 'Arthritis medications',
-    image: 'https://cdn-icons-png.flaticon.com/512/4320/4320337.png',
-    description: '40+ medicines'
+    "name": "Anxiolytics",
+    "image": "https://cdn-icons-png.flaticon.com/512/4149/4149668.png",
+    "description": "20+ medicines"
   },
   {
-    name: 'Asthma medications',
-    image: 'https://cdn-icons-png.flaticon.com/512/4388/4388166.png',
-    description: '30+ products'
+    "name": "Arthritis medications",
+    "image": "https://cdn-icons-png.flaticon.com/512/4149/4149681.png",
+    "description": "40+ medicines"
   },
   {
-    name: 'Burn medications',
-    image: 'https://cdn-icons-png.flaticon.com/512/2751/2751725.png',
-    description: '10+ items'
+    "name": "Asthma medications",
+    "image": "https://cdn-icons-png.flaticon.com/512/2785/2785828.png",
+    "description": "30+ products"
   },
   {
-    name: 'Cholesterol-lowering drugs',
-    image: 'https://cdn-icons-png.flaticon.com/512/7660/7660378.png',
-    description: '25+ medicines'
+    "name": "Burn medications",
+    "image": "https://cdn-icons-png.flaticon.com/512/2785/2785851.png",
+    "description": "10+ items"
   },
   {
-    name: 'Cognitive enhancers',
-    image: 'https://cdn-icons-png.flaticon.com/512/3154/3154587.png',
-    description: '15+ supplements'
+    "name": "Cholesterol-lowering drugs",
+    "image": "https://cdn-icons-png.flaticon.com/512/2936/2936861.png",
+    "description": "25+ medicines"
   },
   {
-    name: 'Contraceptives',
-    image: 'https://cdn-icons-png.flaticon.com/512/4689/4689162.png',
-    description: '20+ products'
+    "name": "Cognitive enhancers",
+    "image": "https://cdn-icons-png.flaticon.com/512/4149/4149657.png",
+    "description": "15+ supplements"
   },
   {
-    name: 'Cough suppressants',
-    image: 'assets/images/icons/lungs-svgrepo-com.svg',
-    description: '18+ medicines'
+    "name": "Contraceptives",
+    "image": "https://cdn-icons-png.flaticon.com/512/2785/2785800.png",
+    "description": "20+ products"
   },
   {
-    name: 'Dermatological drugs',
-    image: 'https://cdn-icons-png.flaticon.com/512/2965/2965573.png',
-    description: '22+ products'
+    "name": "Cough suppressants",
+    "image": "https://cdn-icons-png.flaticon.com/512/869/869869.png",
+    "description": "18+ medicines"
   },
   {
-    name: 'Diuretics',
-    image: 'https://cdn-icons-png.flaticon.com/512/831/831682.png',
-    description: '15+ medicines'
+    "name": "Dermatological drugs",
+    "image": "https://cdn-icons-png.flaticon.com/512/2936/2936870.png",
+    "description": "22+ products"
   },
   {
-    name: 'Erectile dysfunction drugs',
-    image: 'https://cdn-icons-png.flaticon.com/512/2884/2884565.png',
-    description: '12+ products'
+    "name": "Diuretics",
+    "image": "https://cdn-icons-png.flaticon.com/512/2965/2965571.png",
+    "description": "15+ medicines"
   },
   {
-    name: 'Fertility drugs',
-    image: 'https://cdn-icons-png.flaticon.com/512/3523/3523063.png',
-    description: '10+ medicines'
+    "name": "Erectile dysfunction drugs",
+    "image": "https://cdn-icons-png.flaticon.com/512/2785/2785858.png",
+    "description": "12+ products"
   },
   {
-    name: 'Heart failure medications',
-    image: 'https://cdn-icons-png.flaticon.com/512/10746/10746929.png',
-    description: '14+ medicines'
+    "name": "Fertility drugs",
+    "image": "https://cdn-icons-png.flaticon.com/512/2785/2785842.png",
+    "description": "10+ medicines"
   },
   {
-    name: 'Hormone',
-    image: 'https://cdn-icons-png.flaticon.com/512/9953/9953296.png',
-    description: '25+ products'
+    "name": "Heart failure medications",
+    "image": "https://cdn-icons-png.flaticon.com/512/4149/4149675.png",
+    "description": "14+ medicines"
   },
   {
-    name: 'Immunostimulants',
-    image: 'https://cdn-icons-png.flaticon.com/512/4727/4727499.png',
-    description: '8+ products'
+    "name": "Hormone",
+    "image": "https://cdn-icons-png.flaticon.com/512/2785/2785833.png",
+    "description": "25+ products"
   },
   {
-    name: 'Immunosuppressants',
-    image: 'https://cdn-icons-png.flaticon.com/512/4472/4472967.png',
-    description: '9+ medicines'
+    "name": "Immunostimulants",
+    "image": "https://cdn-icons-png.flaticon.com/512/4320/4320346.png",
+    "description": "8+ products"
   },
   {
-    name: 'Laxatives',
-    image: 'https://cdn-icons-png.flaticon.com/512/2965/2965567.png',
-    description: '11+ products'
+    "name": "Immunosuppressants",
+    "image": "https://cdn-icons-png.flaticon.com/512/4320/4320348.png",
+    "description": "9+ medicines"
   },
   {
-    name: 'Menopause medications',
-    image: 'https://cdn-icons-png.flaticon.com/512/1143/1143553.png',
-    description: '7+ medicines'
+    "name": "Laxatives",
+    "image": "https://cdn-icons-png.flaticon.com/512/2917/2917992.png",
+    "description": "11+ products"
   },
   {
-    name: 'Migraine medications',
-    image: 'https://cdn-icons-png.flaticon.com/512/4290/4290854.png',
-    description: '13+ products'
+    "name": "Menopause medications",
+    "image": "https://cdn-icons-png.flaticon.com/512/2785/2785825.png",
+    "description": "7+ medicines"
   },
   {
-    name: 'Mucolytics',
-    image: 'https://cdn-icons-png.flaticon.com/512/3307/3307196.png',
-    description: '6+ medicines'
-  },
-
-  {
-    name: 'NSAID',
-    image: 'https://cdn-icons-png.flaticon.com/512/4320/4320337.png',
-    description: '25+ medicines'
+    "name": "Migraine medications",
+    "image": "https://cdn-icons-png.flaticon.com/512/4149/4149676.png",
+    "description": "13+ products"
   },
   {
-    name: 'Neuropathic pain drugs',
-    image: 'https://cdn-icons-png.flaticon.com/512/4388/4388166.png',
-    description: '8+ medicines'
+    "name": "Mucolytics",
+    "image": "https://cdn-icons-png.flaticon.com/512/2785/2785829.png",
+    "description": "6+ medicines"
   },
   {
-    name: 'Ophthalmic drugs',
-    image: 'https://cdn-icons-png.flaticon.com/512/2751/2751725.png',
-    description: '12+ products'
+    "name": "NSAID",
+    "image": "https://cdn-icons-png.flaticon.com/512/4320/4320316.png",
+    "description": "25+ medicines"
   },
   {
-    name: 'Osteoporosis drugs',
-    image: 'https://cdn-icons-png.flaticon.com/512/7660/7660378.png',
-    description: '10+ medicines'
+    "name": "Neuropathic pain drugs",
+    "image": "https://cdn-icons-png.flaticon.com/512/4149/4149672.png",
+    "description": "8+ medicines"
   },
   {
-    name: 'Otic preparations',
-    image: 'https://cdn-icons-png.flaticon.com/512/3154/3154587.png',
-    description: '7+ products'
+    "name": "Ophthalmic drugs",
+    "image": "https://cdn-icons-png.flaticon.com/512/2947/2947938.png",
+    "description": "12+ products"
   },
   {
-    name: 'Prostate medications',
-    image: 'https://cdn-icons-png.flaticon.com/512/4689/4689162.png',
-    description: '6+ medicines'
-  },
-
-  {
-    name: 'Thyroid medications',
-    image: 'https://cdn-icons-png.flaticon.com/512/2965/2965573.png',
-    description: '8+ medicines'
+    "name": "Osteoporosis drugs",
+    "image": "https://cdn-icons-png.flaticon.com/512/2936/2936877.png",
+    "description": "10+ medicines"
   },
   {
-    name: 'Urinary tract',
-    image: 'https://cdn-icons-png.flaticon.com/512/831/831682.png',
-    description: '9+ products'
+    "name": "Otic preparations",
+    "image": "https://cdn-icons-png.flaticon.com/512/2785/2785847.png",
+    "description": "7+ products"
   },
-
   {
-    name: 'Vasodilators',
-    image: 'https://cdn-icons-png.flaticon.com/512/3523/3523063.png',
-    description: '5+ medicines'
+    "name": "Prostate medications",
+    "image": "https://cdn-icons-png.flaticon.com/512/2785/2785818.png",
+    "description": "6+ medicines"
   },
+  {
+    "name": "Thyroid medications",
+    "image": "https://cdn-icons-png.flaticon.com/512/2785/2785867.png",
+    "description": "8+ medicines"
+  },
+  {
+    "name": "Urinary tract",
+    "image": "https://cdn-icons-png.flaticon.com/512/2785/2785855.png",
+    "description": "9+ products"
+  },
+  {
+    "name": "Vasodilators",
+    "image": "https://cdn-icons-png.flaticon.com/512/4149/4149674.png",
+    "description": "5+ medicines"
+  }
 ];
